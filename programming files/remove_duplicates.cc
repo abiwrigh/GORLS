a67a4951b7e5cf316a06637523a11c706a4dbc35
abiwrigh <112552934+abiwrigh@users.noreply.github.com>
2024-04-11 17:02:20 -0400

    Update remove_duplicates.cc

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -5,28 +5,28 @@ using namespace std;
 
 vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
     // Create new array for unique elements
-    vector<int> unique_arr;
-    sort(arr.begin(), arr.end());
+    vector<int> unique_arr
+    sort(arr.begin(), arr.end())
     // Set n equal to the size of the original array
-    int n = arr.size();
+    int n = arr.size()
     // Go through original array and add unique elements to unique array
     // Unique is determined by if the lement in front of it is or is not equal
     for(int i=0; i<n; i++){
         if(i == 0 || arr[i] != arr[i-1]){
-            unique_arr.push_back(arr[i]);
+            unique_arr.push_back(arr[i])
         }
             // Add removed duplicates to removed array.
         else{
-            removed_elements.push_back(arr[i]);
+            removed_elements.push_back(arr[i])
         }
     }
-    return unique_arr;
+    return unique_arr
 }
 
 int main(){
     // Test the function
     // Create new original vector
-    vector<int> arr = {1, 2, 2, 3, 4, 4, 5};
+    vector<int> arr = {1, 2, 2, 3, 4, 4, 5}
     // Create empty removed elements vector
     vector<int> removed_elements;
     // Create unique vector using original array and calling remove_duplicates
